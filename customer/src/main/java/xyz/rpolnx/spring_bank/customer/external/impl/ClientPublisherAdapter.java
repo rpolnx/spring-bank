c8190dad1b5ac643c027839520f8e25aceedd949
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-26 05:04:05 -0300

    Fixing rabbitmq conversion

--- customer/src/main/java/xyz/rpolnx/spring_bank/customer/external/impl/ClientPublisherAdapter.java
+++ customer/src/main/java/xyz/rpolnx/spring_bank/customer/external/impl/ClientPublisherAdapter.java
@@ -1,11 +1,11 @@
 package xyz.rpolnx.spring_bank.customer.external.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import xyz.rpolnx.spring_bank.customer.external.ClientPublisher;
 import xyz.rpolnx.spring_bank.customer.model.dto.ClientDTO;
@@ -17,14 +17,12 @@ public class ClientPublisherAdapter implements ClientPublisher {
     private final AmqpTemplate amqpTemplate;
     private final ObjectMapper mapper;
 
+    @Value("${customer-queue-name:customer-queue}")
+    private String queue;
+
     @SneakyThrows
     @Override
     public void handleClientCreation(ClientDTO clientDTO) {
-        try {
-            amqpTemplate.convertAndSend(mapper.writeValueAsString(clientDTO));
-        } catch (JsonProcessingException ex) {
-
-            throw ex;
-        }
+        amqpTemplate.convertAndSend(queue, mapper.writeValueAsString(clientDTO));
     }
 }
