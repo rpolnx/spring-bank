966c07467c09804738d19f02c9a491918bb778bd
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-08-26 03:49:42 -0300

    Flyway migration and props

--- customer/src/main/resources/db/migration/V1.0__CREATE_USERS.sql
+++ customer/src/main/resources/db/migration/V1.0__CREATE_USERS.sql
@@ -6,7 +6,7 @@ CREATE TYPE "PersonType" AS ENUM (
 CREATE TABLE "clients" (
   "document_number" varchar(14) PRIMARY KEY,
   "full_name" varchar(100) NOT NULL,
-  "person_type" PersonType,
+  "person_type" "PersonType",
   "score" integer,
   "created_at" timestamp,
   "updated_at" timestamp
